9839e71eed1b0b832ecb8d44d354d1a3e8814d6f
Tristan Matthias <hello@tristanmatthias.com>
2018-03-31 20:31:02 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Typescript, Node.js boilerplate
+
+This repo is meant to be a boilerplate for new Typescript/Node.js modules.
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const PATH_SRC = path.resolve(__dirname, 'src');
+const PATH_DIST = path.resolve(__dirname, '_bundles');
+
+const BitBarPlugin = require('bitbar-webpack-progress-plugin');
+
+module.exports = {
+    entry: {
+        "bundle": path.join(PATH_SRC, 'index.ts')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html/,
+                loader: 'raw-loader'
+            },
+            {
+                test: /\.scss/,
+                loader: 'css-loader!sass-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    output: {
+        filename: '[name].js',
+        path: PATH_DIST,
+        libraryTarget: 'umd',
+        library: 'bundle',
+        umdNamedDefine: true
+    },
+    node: {
+        fs: "empty"
+    },
+    plugins: [
+        new BitBarPlugin()
+    ]
+}
